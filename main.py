e515a555d38a537291135a4882af765197b8cd13
Artur Mustafin <funcelot@outlook.com>
2022-01-21 19:43:03 +0000

    refactor

--- main.py
+++ main.py
@@ -62,16 +62,14 @@ def shuffle_binb(alphabet, str):
   for i in range(16):
     shuffle(alphabet, array[i])
 
-def cipher(cipher, next):
-  def function(shift, alphabet, array, sha_alphabet, sha_plaintext):
+def cipher(f, next, shift, alphabet, array, sha_alphabet, sha_plaintext):
     shuffle_binb(alphabet, sha_alphabet)
     shuffle_binb(alphabet, sha_plaintext)
     i = 0
     while i < shift:
-      array = [*map(cipher(alphabet, next), array)]
+      array = [*map(f(alphabet, next), array)]
       i = i + 1
     return array
-  return function
 
 def caesar(alphabet, next):
   def enc(char):
@@ -83,7 +81,7 @@ def caesar(alphabet, next):
   return enc
 
 def caesar_cipher(next, shift, alphabet, plaintext, sha_alphabet, sha_plaintext):
-  return cipher(caesar, next)(shift, alphabet, plaintext, sha_alphabet, sha_plaintext)
+  return cipher(caesar, next, shift, alphabet, plaintext, sha_alphabet, sha_plaintext)
 
 alphabet = [chr(c) for c in [126, 33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 95, 43, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 123, 125, 124, 65, 83, 68, 70, 71, 72, 74, 75, 76, 58, 34, 90, 88, 67, 86, 66, 78, 77, 60, 62, 63, 96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 32, 10, 1049, 1062, 1059, 1050, 1045, 1053, 1043, 1064, 1065, 1047, 1061, 1066, 1060, 1067, 1042, 1040, 1055, 1056, 1054, 1051, 1044, 1046, 1069, 1071, 1063, 1057, 1052, 1048, 1058, 1068, 1041, 1070, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102]]
 
@@ -96,5 +94,5 @@ rnd = prng(seed)
 
 encoded = [chr(c) for c in [43, 1103, 1094, 1087, 58, 1060, 63, 33, 80, 1070, 1042, 79, 1094, 45, 1071, 110, 1042, 1094, 71, 41, 97, 10, 108, 1078, 68, 75, 96, 1084, 1089, 1083, 1044, 109, 1060, 1095, 49, 10, 61, 79, 1055, 1045, 1074, 109, 68, 1056, 64, 1069, 40, 88, 114, 1092, 74, 77, 124, 90, 1062, 1087, 1050, 1055, 118, 84, 55, 1094, 68, 84, 1074, 101, 81, 126, 86, 121, 63, 39, 69, 1069, 57, 1052, 108, 57, 1088, 64, 1094, 117, 1046, 1079, 102, 1057, 1058, 102, 1094, 1046, 1100, 76, 1097, 1081, 68, 1060, 90]]
 
-decoded = caesar_cipher(decode, datetime.now().year, alphabet, encoded, "7557f484cc389712fabf1de974e17636540d44c1b65884df8cf97b9ebdbcddf61846639a19a19113881e3727c8aff3e55e85a0cbe8794edf4a2e1857d07ea6f8", "367de9fdbd996645f973e0d19b2ea8935bd4ff8f3f145ac5f440682903a74040f6a579e67b08b81522ffab930d63e2293d4ee2580a8db4090c53c714c8a7e33c")
+decoded = cipher(caesar, decode, datetime.now().year, alphabet, encoded, "7557f484cc389712fabf1de974e17636540d44c1b65884df8cf97b9ebdbcddf61846639a19a19113881e3727c8aff3e55e85a0cbe8794edf4a2e1857d07ea6f8", "367de9fdbd996645f973e0d19b2ea8935bd4ff8f3f145ac5f440682903a74040f6a579e67b08b81522ffab930d63e2293d4ee2580a8db4090c53c714c8a7e33c")
 print("".join(decoded))
